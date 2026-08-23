3ec8adbd7b0c4b6d0fde92a0c9adca0107698254
kinikv <kinikv@gmail.com>
2026-08-23 15:30:02 +0530

    Add 80_Type_1_Fn.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/80_Type_1_Fn.js
@@ -0,0 +1,11 @@
+function greet(){
+    console.log("Hello, World!");
+
+}
+
+greet();
+greet();
+greet();
+
+let output = greet();
+console.log(output); // undefined, because the function does not return anything

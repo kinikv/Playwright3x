f412a6864c7aa83de2f608b7989aa0d29d2b539d
kinikv <kinikv@gmail.com>
2026-08-23 15:46:49 +0530

    Add 82_Type_3_Fn.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/82_Type_3_Fn.js
@@ -0,0 +1,9 @@
+function greet() {
+    console.log("Hi there!");
+    return "Hello, World!";
+}
+
+let output = greet();
+console.log(output); // "Hello, World!", because the function returns a value
+
+

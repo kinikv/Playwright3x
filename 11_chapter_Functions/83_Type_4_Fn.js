c135d9aa57b4ef8545500b1583c99b0572740e51
kinikv <kinikv@gmail.com>
2026-08-23 15:51:08 +0530

    Add 83_Type_4_Fn.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/83_Type_4_Fn.js
@@ -0,0 +1,6 @@
+function sumOfNumbers(a, b) {
+    return a + b;
+}
+let result = sumOfNumbers(5, 10);
+console.log(result); // 15, because the function returns the sum of a and b
+console.log(sumOfNumbers(20, 30)); // 50, because the function returns the sum of a and b

3862943ca250c63d454d4161d6c04fb3a245c7be
kinikv <kinikv@gmail.com>
2026-08-23 16:11:27 +0530

    Add 85_Fn_Exp.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/85_Fn_Exp.js
@@ -0,0 +1,10 @@
+function greet1(name1) {
+    return `Hello, ${name1}!`;
+}
+
+const greet=function(name){
+    return `Hello, ${name}!`;
+}
+
+console.log(greet1("Alice"));
+console.log(greet("Bob"));

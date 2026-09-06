d255756410399a4998a44acea93a8aa3c812c7d6
kinikv <kinikv@gmail.com>
2026-09-06 14:20:10 +0530

    Add 92_Return_fn.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/92_Return_fn.js
@@ -0,0 +1,16 @@
+let a=10;
+console.log(a);
+
+function printTest(){
+    console.log("Hello Student");
+    let a=30;
+    console.log(a);
+    if(true){
+        let a=5;
+        console.log('a->',a);
+    }
+    console.log(a);
+}
+
+printTest();
+console.log(a);

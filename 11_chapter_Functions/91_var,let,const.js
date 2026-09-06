fc659acfb269257effe38b66f4dee7296d63621a
kinikv <kinikv@gmail.com>
2026-09-06 14:40:45 +0530

    Add 91_var,let,const.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 11_chapter_Functions/91_var,let,const.js
@@ -0,0 +1,15 @@
+var a=10;
+console.log(a);
+
+function printTest(){
+    console.log("Hello Student");
+    var a=20;
+    console.log(a);
+    if(true){
+        var a=30;
+        console.log(a);
+    }
+    console.log(a);
+}
+
+printTest();
